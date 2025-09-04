2055d52dd6a6a84e5ff3834677f1a6e83d7646d8
Yichen <unncchen@gmail.com>
2025-09-04 17:09:44 +0100

    Update Monte Carlo Simulation.py

--- VaR Estimation (Real Data)/Monte Carlo Simulation.py
+++ VaR Estimation (Real Data)/Monte Carlo Simulation.py
@@ -2,29 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# load data
 file_path = "CAC40 data.xlsx"
 df = pd.read_excel(file_path, skiprows=2)
 df.columns = ['Date', 'Closing Price', 'Returns', 'Sorted Returns']
 df['Returns'] = pd.to_numeric(df['Returns'], errors='coerce')
 returns = df['Returns'].dropna()
 
-# parameter
 mu = returns.mean()
 sigma = returns.std()
-n_simulations = 10000  # Simulation number
-holding_days = 1       # holding period（1 day）
+n_simulations = 10000  
+holding_days = 1       
 alpha_95 = 0.05
 alpha_99 = 0.01
 alpha_975 = 0.025
 
-# Monte Carlo Simulation
 simulated_returns = np.random.normal(loc=mu * holding_days,
                                      scale=sigma * np.sqrt(holding_days),
                                      size=n_simulations)
-
-#z = np.random.normal(0, 1, n_simulations)  # Z_i ~ N(0,1)
-#simulated_returns = mu * holding_days + sigma * np.sqrt(holding_days) * z
 
 # calculate VaR
 mc_var_95 = -np.percentile(simulated_returns, alpha_95 * 100)
